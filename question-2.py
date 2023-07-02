f1bd941dd203381ba8a96b70068541bed81a0af8
1999Arathy <85410988+1999Arathy@users.noreply.github.com>
2023-07-02 20:41:56 +0530

    Add files via upload

---
+++ question-2.py
@@ -0,0 +1,3 @@
+str1 = "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
+x = str1.count("n")
+print("Frequency of character 'n' is : {0}".format(x)) 
